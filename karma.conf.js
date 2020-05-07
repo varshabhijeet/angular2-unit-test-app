f890802f3be468f28a12dea0141352d024a89764
varshabhijeet <47491842+varshabhijeet@users.noreply.github.com>
2020-05-07 12:00:15 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['phantomjs'],
     captureTimeout: 6000,
     singleRun: true
   })
